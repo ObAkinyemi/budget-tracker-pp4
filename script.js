2c20e0513cc5b2a00477ea205e083c09facb185a
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-04-07 22:46:53 -0600

    got budget input working

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// console.log(budget);
+
+function display_budget () {
+    let budget = document.getElementById("BudgetInput");
+    console.log(typeof(budget.click));
+
+    // if (budget.click == true) {
+    //     console.log(Number(budget.value));
+    // }
+
+}
+function expense () {
+    let expense
+}
+
+function liability () {
+
+}
+
+
+function expense_liability_information() {
+    
+}
+
+function total_spent () {
+
+}
+
+function total_invested () {
+
+}
+
+function net_worth () {
+
+}
