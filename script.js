e3235d07d536ff51157946bf0b1d41438a286cb8
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-04-08 22:35:09 -0600

    got the expense and investment information grabber/storer working with help from Chat

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 // console.log(budget);
-expense_storage = [];
+let expense_storage = [];
+let investment_storage = [];
+// let i_name = document.getElementById("expense_name");
+// let worth = document.getElementById("expense_worth");
+// let category = document.getElementById("expense_type");
+// let expense_button = document.getElementById("add_exp_button");
 
 function display_budget () {
     let budget = document.getElementById("BudgetInput");
@@ -11,24 +16,33 @@ function display_budget () {
     // }
 
 }
-function expense (name, worth, category) {
-    // let name = document.getElementById("expense_name");
-    // let worth = document.getElementById("expense_worth");
-    // let category = document.getElementById("expense_type");
-    // let expense_button = document.getElementById("add_exp_button");
-    console.log(name);
-    console.log(worth);
-    console.log(category);
-    return {name, worth, category};
-}
 
-function store_expense () {
-    expense_storage.push(expense());
-    console.log(expense_storage);
+function handleAddExpense() {
+    let i_name = document.getElementById("expense_name");
+    let i_worth = document.getElementById("expense_worth");
+    let i_cat = document.getElementById("expense_type");
+    let expense = {
+        name: i_name.value,
+        worth: i_worth.value,
+        cat: i_cat.value
+    }
+    expense_storage.push(expense);
+    console.log("expenses", expense_storage);
     return expense_storage;
 }
-function liability () {
 
+function handleAddInvestment () {
+    let i_name_inv = document.getElementById("investment_name");
+    let i_worth_inv = document.getElementById("investment_worth");
+    let i_cat_inv = document.getElementById("investment_type");
+    let investment = {
+        name: i_name_inv.value,
+        worth: i_worth_inv.value,
+        cat: i_cat_inv.value
+    }
+    investment_storage.push(investment);
+    console.log("investments", investment_storage);
+    return investment_storage;
 }
 
 
