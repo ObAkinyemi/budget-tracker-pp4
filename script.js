8ea3efa703c3f8938547e34c1cd4105ccddaed82
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-04-22 11:53:20 -0600

    idk where I'm at rn, it's been too long.

--- script.js
+++ script.js
@@ -2,19 +2,37 @@
 // console.log(budget);
 let expense_storage = [];
 let investment_storage = [];
+let budget = document.getElementById("BudgetInput");
+function doc_get_id (id) {
+    return document.getElementById(id);
+}
+let total_expenses = 0;
 // let i_name = document.getElementById("expense_name");
 // let worth = document.getElementById("expense_worth");
 // let category = document.getElementById("expense_type");
 // let expense_button = document.getElementById("add_exp_button");
 
-function display_budget () {
-    let budget = document.getElementById("BudgetInput");
-    console.log(typeof(budget.click));
+function handleBudget () {
 
+    console.log(budget.value);
+    updateBudget();
+    
     // if (budget.click == true) {
     //     console.log(Number(budget.value));
     // }
 
+}
+function updateBudget () {
+    budget_disp_text = document.getElementById("budget-disp").innerText
+    // do {
+    budget_disp_text = `Current Budget: ${budget.value}`;
+
+    // } while ();
+
+}
+
+function sum_expense_investment (expense_quant, investment_quant) {
+    //
 }
 
 function handleAddExpense() {
@@ -27,8 +45,19 @@ function handleAddExpense() {
         cat: i_cat.value
     }
     expense_storage.push(expense);
-    console.log("expenses", expense_storage);
+    // console.log("expenses", expense_storage);
+    console.log(expense_storage);
+
+    for (let i of expense_storage) {
+        for (let exp in i.worth){
+            total_expenses += 1;
+            console.log (total_expenses);
+        }
+    }
+    console.log(`worth: ${expense_storage[0].worth}`);
+    console.log(`total expenses ${total_expenses}`);
     return expense_storage;
+
 }
 
 function handleAddInvestment () {
