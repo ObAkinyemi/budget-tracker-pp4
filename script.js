20bbfb6d5d1de146dd8a80945bbd7095616dcf5f
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-04-27 07:13:51 -0600

    My code isn't working. I'm able to get my budget displayed, however the logic for calculated the budget after the user inputs expenses or investments is not working properly

--- script.js
+++ script.js
@@ -1,92 +1,81 @@
-
-// console.log(budget);
+// Correctly initialize empty storages
 let expense_storage = [];
 let investment_storage = [];
 let budget = document.getElementById("BudgetInput");
-function doc_get_id (id) {
-    return document.getElementById(id);
-}
 let total_expenses = 0;
-// let i_name = document.getElementById("expense_name");
-// let worth = document.getElementById("expense_worth");
-// let category = document.getElementById("expense_type");
-// let expense_button = document.getElementById("add_exp_button");
+const expenseButton = document.getElementById("add_exp_button");
+expenseButton.addEventListener("click", net_Budget);
+// Don't call expense_tot or invest_tot immediately!
+// They will be called later, after user adds data.
 
-function handleBudget () {
-
+function handleBudget() {
     console.log(budget.value);
-    updateBudget();
-    
-    // if (budget.click == true) {
-    //     console.log(Number(budget.value));
-    // }
+    const net = net_Budget(expense_storage, investment_storage);
 
-}
-function updateBudget () {
-    budget_disp_text = document.getElementById("budget-disp").innerText
-    // do {
-    budget_disp_text = `Current Budget: ${budget.value}`;
+    console.log("net budget:", net);
 
-    // } while ();
-
+    document.getElementById("budget-disp").innerText = 
+      `Current Budget: ${budget.value}\n
+      Net Budget after expenses/investments: ${net}`;
 }
 
-function sum_expense_investment (expense_quant, investment_quant) {
-    //
+
+function net_Budget(expenses, investments) {
+    const sumExpenses = expenses.reduce((total, expense) => total + Number(expense.worth), 0);
+    console.log("total expenses", sumExpenses);
+    const suminvestments = investments.reduce((total, investment) => total + Number(investment.worth), 0);
+    console.log("total investments", suminvestments);
+    return suminvestments-sumExpenses;
+
+}
+
+function invest_tot(arr, key) {
+    return arr.map(obj => obj[key]);
+}
+
+function expense_tot(arr, key) {
+    // console.log(arr.map(obj => obj[key]));
+    return arr.map(obj => obj[key]);
 }
 
 function handleAddExpense() {
     let i_name = document.getElementById("expense_name");
     let i_worth = document.getElementById("expense_worth");
     let i_cat = document.getElementById("expense_type");
+
     let expense = {
         name: i_name.value,
         worth: i_worth.value,
         cat: i_cat.value
-    }
+    };
+
     expense_storage.push(expense);
-    // console.log("expenses", expense_storage);
-    console.log(expense_storage);
+    console.log("Expenses", expense_storage);
 
-    for (let i of expense_storage) {
-        for (let exp in i.worth){
-            total_expenses += 1;
-            console.log (total_expenses);
-        }
-    }
-    console.log(`worth: ${expense_storage[0].worth}`);
-    console.log(`total expenses ${total_expenses}`);
+    // Now safely calculate total worths
+    const expense_values = expense_tot(expense_storage, 'worth');
+    console.log("Expense values:", expense_values);
+
     return expense_storage;
-
 }
 
-function handleAddInvestment () {
+function handleAddInvestment() {
     let i_name_inv = document.getElementById("investment_name");
     let i_worth_inv = document.getElementById("investment_worth");
     let i_cat_inv = document.getElementById("investment_type");
+
     let investment = {
         name: i_name_inv.value,
         worth: i_worth_inv.value,
         cat: i_cat_inv.value
-    }
+    };
+
     investment_storage.push(investment);
-    console.log("investments", investment_storage);
+    console.log("Investments", investment_storage);
+
+    // Now safely calculate total worths
+    const invest_values = invest_tot(investment_storage, 'worth');
+    console.log("Investment values:", invest_values);
+
     return investment_storage;
-}
-
-
-function expense_liability_information() {
-    
-}
-
-function total_spent () {
-
-}
-
-function total_invested () {
-
-}
-
-function net_worth () {
-
 }
